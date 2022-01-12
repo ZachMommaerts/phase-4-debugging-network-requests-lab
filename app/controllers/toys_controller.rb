a2b813b0d468b953312e1dd809f2d14def921c34
Zach <zmommaerts@gmail.com>
2022-01-11 20:16:28 -0600

    completed assignment

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -6,14 +6,19 @@ class ToysController < ApplicationController
     render json: toys
   end
 
+  def show
+    render json: Toy.find(params[:id])
+  end
+
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy
   end
 
   def destroy
